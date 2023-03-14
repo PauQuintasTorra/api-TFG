c7842bc57e643bcfebbcaf8374afc308618805ac
Pau Quintas <pau.quintas@e-campus.uab.cat>
2023-03-14 01:57:10 +0100

    functional image sender

--- api.js
+++ api.js
@@ -12,13 +12,6 @@ app.get('/api/data', (req, res) => {
   res.send(tx_json);
 });
 
-app.get('/api/image', (req, res) => {
-  const imagePath = 'path/to/image.jpg';
-  const image = fs.readFileSync(imagePath);
-  const base64Image = Buffer.from(image).toString('base64');
-  res.send({ image: base64Image });
-});
-
 app.post('/api/uploadImage', upload.single('image'), (req,res)=> {
   console.log(req.file);
   const binaryData = fs.readFileSync(req.file.path);
@@ -29,11 +22,14 @@ app.post('/api/uploadImage', upload.single('image'), (req,res)=> {
   .then(jpgData => {
     // Write the JPG data to a file
     fs.writeFileSync('image.jpg', jpgData);
+    const imagePath = './image.jpg';
+    const image = fs.readFileSync(imagePath);
+    const base64Image = Buffer.from(image).toString('base64');
+    res.send({ image: base64Image });
   })
   .catch(err => {
     console.error(err);
   });
-  res.send(JSON.stringify("Arriba al back!"));
 })
 
 // Iniciar el servidor
