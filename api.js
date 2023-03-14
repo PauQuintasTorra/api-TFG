b0de88fc48a7876af9ea89a3e5624d24dc0dd4d2
Pau Quintas <pau.quintas@e-campus.uab.cat>
2023-03-14 01:21:15 +0100

    sharp + trying to send photo

--- api.js
+++ api.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const multer = require('multer');
 const upload = multer({dest: 'uploads/'});
+const fs = require('fs');
+const sharp = require('sharp');
 
 // Ruta para enviar una respuesta al cliente de Angular
 app.get('/api/data', (req, res) => {
@@ -10,9 +12,27 @@ app.get('/api/data', (req, res) => {
   res.send(tx_json);
 });
 
+app.get('/api/image', (req, res) => {
+  const imagePath = 'path/to/image.jpg';
+  const image = fs.readFileSync(imagePath);
+  const base64Image = Buffer.from(image).toString('base64');
+  res.send({ image: base64Image });
+});
+
 app.post('/api/uploadImage', upload.single('image'), (req,res)=> {
-  console.log(req.body);
   console.log(req.file);
+  const binaryData = fs.readFileSync(req.file.path);
+  console.log(binaryData);
+  sharp(binaryData)
+  .toFormat('jpeg')
+  .toBuffer()
+  .then(jpgData => {
+    // Write the JPG data to a file
+    fs.writeFileSync('image.jpg', jpgData);
+  })
+  .catch(err => {
+    console.error(err);
+  });
   res.send(JSON.stringify("Arriba al back!"));
 })
 
